02170f4ef3c14b842164216bffda716e341a234e
rachine <riadrachid3@gmail.com>
2016-12-02 17:30:24 +0100

    Image set, installation instructions

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ExposureFusion
 Exposure Fusion Technique
+
+This Technique used OpenCV 3 and python 2.7 and is based on this [Tutorial](http://www.pyimagesearch.com/2015/06/22/install-opencv-3-0-and-python-2-7-on-ubuntu/).
---
+++ image.py
@@ -0,0 +1,9 @@
+
+
+class Image(object):
+    """docstring for Image."""
+    def __init__(self, arg):
+        super(Image, self, fmt,path).__init__()
+        self.path = "image_set/"+fmt+'/'+str(path)
+        self.fmt = fmt
+        
